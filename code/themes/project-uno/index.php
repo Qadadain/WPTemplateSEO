ba1cc474b87ba5832bd96c3bfb0fb98920ed8b13
Quentin ADADAIN <quentin.adadain@gmail.com>
2024-05-25 17:20:47 +0200

    Add article list on homepage and  aside

--- code/themes/project-uno/index.php
+++ code/themes/project-uno/index.php
@@ -1,9 +1,58 @@
 <?php get_header(); ?>
-    <div class="mt-fixed">
-<h1>Ceci est un titre</h1>
+<?php
+// get latest posts
+$latest_posts = get_posts([
+    'numberposts' => 10,
+    'orderby' => 'date',
+    'order' => 'DESC',
+    'post_type' => 'post',
+    'post_status' => 'publish'
+]);
+?>
+    <main class="mt-fixed">
+        <div class="home-container">
+            <section>
+                <div>
+                    <?php $counter = 0;
+                    foreach ($latest_posts as $post):
+                        $counter++;
+                        if ($counter <= 2 || ($counter > 5 && $counter <= 7)) {
+                            $article_class = "big-article";
+                        } else {
+                            $article_class = "medium-article";
+                        }
+                        ?>
+                        <article class="<?php echo $article_class; ?>">
+                            <div class="header-article">
+                                <div class="category">
+                                    <span><?php echo get_the_category($post->ID)[0]->cat_name; ?></span>
+                                </div>
+                                <h2><?php echo $post->post_title; ?></h2>
+                            </div>
+                            <figure class="figure">
+                                <img src="<?php echo get_the_post_thumbnail_url($post); ?>" alt="<?php echo $post->post_title; ?>">
+                            </figure>
+                            <div class="footer-article">
+                                <?php echo get_the_excerpt($post); ?>
+                            </div>
+                        </article>
+                    <?php endforeach; ?>
+                </div>
+            </section>
+            <aside>
+                <div class="sticky-aside">
+                aside
 
-        <?php
+                </div>
+                <?php //ICI sidebar ?>
 
-        ?>
-    </div>
+            </aside>
+        </div>
+        <section>
+            <div>
+                <?php //ICI Autres ?>
+
+            </div>
+        </section>
+    </main>
 <?php get_footer();
